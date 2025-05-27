a82a5901fd48bd9f9926d4e54dcd985e8a951f74
Waheed Afolabi <waheed.freelancer16@gmail.com>
2025-05-27 13:16:15 +0100

    Initial Commit

---
+++ MintynTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MintynTask
+//
+//  Created by waheedCodes on 27/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
