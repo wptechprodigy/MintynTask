71b1259b4bd0b4e6886bc8888ee7fb64136c82bf
Waheed Afolabi <waheed.freelancer16@gmail.com>
2025-05-28 13:30:04 +0100

    WIP: Add a fake service to get profile stats

---
+++ MintynTask/Network/ProfileStatService.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileStatService.swift
+//  MintynTask
+//
+//  Created by waheedCodes on 28/05/2025.
+//
+
+import Foundation
+
+struct ProfileStats {
+    let profilePicture: String
+    let growingPlace: String
+    let location: String
+    let years: Int
+    let helps: Int
+    let pounds: Int
+    let numberOfPlants: Int
+    let typeOfPlants: [String]
+}
+
+class ProfileService {
+    static func fetchProfile(completion: @escaping (ProfileStats) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2.0) {
+            let profile = ProfileStats(
+                profilePicture: "rita-malcok",
+                growingPlace: "Outdoor",
+                location: "Sanremo, Italy",
+                years: 15,
+                helps: 698,
+                pounds: 535,
+                numberOfPlants: 100,
+                typeOfPlants: [
+                    "Cloni",
+                    "Tecolote"
+                ])
+            completion(profile)
+        }
+    }
+}
